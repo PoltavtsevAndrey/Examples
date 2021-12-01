25971a98f3d291ddec120bc85198400664663345
Andrey Po <nordman_box@mail.ru>
2021-12-02 00:05:53 +0700

    Example012_ChangeText project

---
+++ Example012_ChangeText/Program.cs
@@ -0,0 +1,29 @@
+﻿string text = "В минуты отъезда и перемены жизни на людей," 
+            + "способных обдумывать свои поступки, обыкновенно находит"
+            + "серьезное настроение мыслей. В эти минуты обыкновенно"
+            + "поверяется прошедшее и делаются планы будущего.";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int leght = text.Length;
+    for (int i = 0; i < leght; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'о', 'О');
+Console.WriteLine(newText);
+Console.WriteLine();
+
